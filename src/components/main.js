75008c894df2d6a5905e4b90d4b5402a4acba64b
Kristina Morozova <christina.vadi@gmail.com>
2023-04-30 19:47:13 +0600

    feat: add description

--- src/components/main.js
+++ src/components/main.js
@@ -3,11 +3,18 @@ export default function createMain() {
   const wrapper = document.createElement('div');
   const textarea = document.createElement('textarea');
   const description = document.createElement('div');
+  const textWin = document.createElement('p');
+  const textLng = document.createElement('p');
 
   main.classList.add('main');
   wrapper.classList.add('wrapper');
   textarea.classList.add('textarea');
   description.classList.add('description');
+  
+  textWin.innerText = 'Cоздана в Windows';
+  textLng.innerText = 'Переключение языка: Левый Alt + Левый Shift';
+  description.appendChild(textWin);
+  description.appendChild(textLng);
 
   main.appendChild(wrapper);
   wrapper.appendChild(textarea);
