615b1fb8dd49a4f407a14f09f5bc612a6120f388
Kristina Morozova <christina.vadi@gmail.com>
2023-04-30 16:25:12 +0600

    feat: add textarea

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,17 @@
 export default function createMain() {
   const main = document.createElement('main');
   const wrapper = document.createElement('div');
+  const textarea = document.createElement('textarea');
+  const description = document.createElement('div');
 
   main.classList.add('main');
   wrapper.classList.add('wrapper');
+  textarea.classList.add('textarea');
+  description.classList.add('description');
 
   main.appendChild(wrapper);
+  wrapper.appendChild(textarea);
+  wrapper.appendChild(description);
 
   return main;
 }
